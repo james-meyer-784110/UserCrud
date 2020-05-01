684c2493b0cd0b5583ffb8baa39eae014f70deca
james-meyer-784110 <james.meyer@cognizant.com>
2020-05-01 10:59:16 -0500

    Added user entity

---
+++ src/main/java/product/crud/usercrud/models/User.java
@@ -0,0 +1,52 @@
+package product.crud.usercrud.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+}
