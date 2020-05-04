a5dcff27c23220eb75ffb592ef1a0172690ac756
james-meyer-784110 <james.meyer@cognizant.com>
2020-05-04 14:52:18 -0500

    Added custom query

--- src/main/java/product/crud/usercrud/repo/UserRepository.java
+++ src/main/java/product/crud/usercrud/repo/UserRepository.java
@@ -1,8 +1,19 @@
 package product.crud.usercrud.repo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import product.crud.usercrud.models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM users WHERE users.name = :username AND users.password = :password",
+            nativeQuery = true)
+    User findByUsernameAndPassword(
+            @Param("username") String username,
+            @Param("password") String password
+    );
 
 }
